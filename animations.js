b00b26051432edf0fe5acd90a02f9ab6a05cfd30
Prateek Mishra <75943911+Prateek692@users.noreply.github.com>
2022-01-26 15:16:25 +0530

    commit2

--- animations.js
+++ animations.js
@@ -1,6 +1,6 @@
 var bar=document.getElementsByTagName("progress");
 var no_of_bars=bar.length;
-for (let i = 0; i < no_of_bars; i++){
+for (let i = 0; i <= no_of_bars; i++){
     var percent=1;
     var interval=setInterval(bar_animation, 10);
     const origin_percent=bar[i].value;
